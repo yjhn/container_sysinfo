3fe7807b940bcba15fb14ea45cc8685e0df11d8c
yjhn <andrius.puksta@gmail.com>
2024-10-07 21:22:22 +0300

    add server that calls the stats server

---
+++ server_go/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const rustServerAddr = "http://localhost:8198/"
+
+func getInfo(w http.ResponseWriter, r *http.Request) {
+	res, err := http.Get(rustServerAddr)
+	if err != nil {
+		fmt.Printf("error making request to %s:\n%s", rustServerAddr, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	info, err2 := io.ReadAll(res.Body)
+	if err2 != nil {
+		fmt.Printf("error reading response from %s:\n%s", rustServerAddr, err2)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+
+	}
+	io.WriteString(w, string(info))
+}
+
+func main() {
+	http.HandleFunc("/", getInfo)
+	http.ListenAndServe(":8199", nil)
+}
